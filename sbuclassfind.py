661a2b16a306e682741f5a5bda680080cd6eb85b
Jenna Mui <Jenna.mui@stonybrook.edu>
2022-10-10 03:12:28 -0400

    update

--- sbuclassfind.py
+++ sbuclassfind.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-page = requests.get('https://www.stonybrook.edu/sb/bulletin/current/search/bysbc/index.php')
+page = requests.get('http://classfind.stonybrook.edu/vufind/Search/Results?lookfor=&type=AllFields&submit=Find')
 
 #create BeautifulSoup object
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -12,32 +12,33 @@ soup = BeautifulSoup(page.text, 'html.parser')
 print(soup.prettify())
 
 #Get Course Names
-titles = soup.find_all('div',class_='webny-teaser-title')
-output_titles = []
-for i in titles: #for x in y: 
+names = soup.find_all('div',class_='resultItemLine1')
+course_names = []
+for i in names: 
     print(i.text)
     data = i.text
-    output_titles.append(data)
+    course_names.append(data)
 
-len(output_titles)
 
-#Get Course Descriptions
-descriptions = soup.find_all('div',class_='webny-card-description')
-output_descriptions = []
-for i in descriptions: 
+len(course_names)
+
+#Get Course Professor
+professor = soup.find_all('div',class_='resultItemLine2')
+course_professor = []
+for i in professor: 
     print(i.text)
     data = i.text
-    output_descriptions.append(data)
+    course_professor.append(data)
 
 #create lists
-list1= output_titles
-list2= output_descriptions
+list1= course_names
+list2= course_professor
 
 #create new dictionary with new lists
-dictionary = {'names': list1, 'descriptions': list2}
+dictionary = {'names': list1, 'time': list2}
 
 #create new dataframe
-df = pd.DataFrame({'names':output_titles,'descriptions': output_descriptions})
+df = pd.DataFrame({'names':course_names,'descriptions': course_professor})
 
 #save dataframe to csv file
-df.to_csv('/Users/jennamui/Documents/GitHub/web-scraping/cdc_website_news.csv')
+df.to_csv('/Users/jennamui/Documents/GitHub/web-scraping/sbu_classfind.csv')
